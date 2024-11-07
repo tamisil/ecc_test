d733b8c82878e80d7c0e4f3508da90c3da6e09f3
Tamara Lehman <53117543+tamisil@users.noreply.github.com>
2024-11-07 16:04:50 -0700

    Create phase9_test.s

---
+++ src/phase9_test.s
@@ -0,0 +1,30 @@
+/*
+ * phase9_test.s
+ *
+ * Author:
+ * Date:
+ *
+ */
+
+ // Section .crt0 is always placed from address 0
+	.section .crt0, "ax"
+
+_start:
+	.global _start
+    // Add test program here
+
+
+
+
+
+    // End of test sequence
+    FINISH:
+        nop
+        nop
+        nop
+        nop
+        nop
+        halt
+        nop
+        nop
+        nop
